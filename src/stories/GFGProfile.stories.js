68f6993110e923c6ef9b0301efeb17cfb47d4845
Bama Charan Chhandogi <b.c.chhandogi@gmail.com>
2024-03-23 23:21:09 +0530

    GFG Components

---
+++ src/stories/GFGProfile.stories.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Meta } from "@storybook/react";
+
+import { GFGProfile } from "../components/GFGProfile";
+import './GFGProfile.css'
+
+export default {
+  title: "App Test",
+  component: GFGProfile,
+  parameters: {
+    // add parameters if needed
+  },
+} as Meta;
+
+export const App = () => <GFGProfile username={"bamacharan"} />;
