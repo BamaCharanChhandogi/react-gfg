faad29cc4831faca26b5e25a26b05c117661d6ad
Bama Charan Chhandogi <b.c.chhandogi@gmail.com>
2024-04-19 23:38:58 +0530

    fix UI

--- src/stories/GFGProfile.stories.js
+++ src/stories/GFGProfile.stories.js
@@ -12,4 +12,4 @@ export default {
   },
 } as Meta;
 
-export const App = () => <GFGProfile username={"bamacharan"} img="https://media.licdn.com/dms/image/D5603AQGj1Vs1ZRE6hQ/profile-displayphoto-shrink_800_800/0/1705839123866?e=2147483647&v=beta&t=EnTSIh_M1Q_J6CDHeUdElbdH-nOBhmvuXC7UifisZfE" showImg={"none"}/>;
+export const App = () => <GFGProfile username={"bamacharan"}/>;
